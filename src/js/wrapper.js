eae2100d7ae3a0d6f054f5ce26863d5c559f6d7b
elusivecodes <18050480+elusivecodes@users.noreply.github.com>
2021-07-10 00:04:34 +1000

    Added DOM to wrapper and cleaned up demos

--- src/js/wrapper.js
+++ src/js/wrapper.js
@@ -1,5 +1,5 @@
 /**
- * FrostUI-SelectMenu v1.0.6
+ * FrostUI-SelectMenu v1.0.7
  * https://github.com/elusivecodes/FrostUI-SelectMenu
  */
 (function(global, factory) {
@@ -23,6 +23,7 @@
     }
 
     const Core = window.Core;
+    const DOM = window.DOM;
     const dom = window.dom;
     const UI = window.UI;
     const document = window.document;
